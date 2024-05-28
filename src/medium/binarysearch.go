395ea593d0e512e4f1633a75d312f9d9880726a9
Junyi Wang <junyi.wang.007@gmail.com>
2024-05-27 22:12:15 -0600

    binary search medium

--- src/medium/binarysearch.go
+++ src/medium/binarysearch.go
@@ -1,5 +1,10 @@
 package medium
 
+import (
+	"math"
+	"slices"
+)
+
 // 74. Search a 2D Matrix
 func SearchMatrix(matrix [][]int, target int) bool {
     sr, er := 0, len(matrix)-1
@@ -44,3 +49,189 @@ func binarySearch(arr []int, t int) int {
     return -1
 }
 
+// 875. Koko Eating Bananas
+func MinEatingSpeed(piles []int, h int) int {
+    canEatUp := func(k int) bool {
+        th := 0
+        for _, p := range piles {
+            th += int(math.Ceil(float64(p) / float64(k)))
+        }
+        return th <= h
+    }
+
+    // binary search
+    s, e := 1, slices.Max(piles)
+    var k int
+    for s <= e {
+        m := (e + s) / 2
+        if canEatUp(m) {
+            // if can eat up in time update k
+            k = m
+            // continue into left subarray (decrease eating speed)
+            e = m-1
+        } else {
+            // otherwise to right subarray (increase eating speed)
+            s = m+1
+        }
+    }
+
+    return k
+}
+
+// 153. Find Minimum in Rotated Sorted Array
+func FindMin(nums []int) int {
+    l, r := 0, len(nums)-1
+    mn := nums[0]
+    for l <= r {
+        if nums[l] < nums[r] {
+            return min(mn, nums[l])
+        }
+        m := (l + r) / 2
+        mn = min(mn, nums[m])
+        if nums[l] <= nums[m] {
+            // m in rotated subarray, search right
+            l = m + 1
+        } else {
+            // m in original subarray, search left
+            r = m - 1
+        }
+    }
+    return mn
+}
+
+func FindMinFast(nums []int) int {
+    l, r, m, result := 0, len(nums) - 1, 0, nums[0]
+
+	for l <= r {
+        m = (l + r) / 2
+		if result > nums[m] {
+			result = nums[m]
+		}
+
+        if nums[l] < nums[m] && nums[l] < nums[r] || nums[l] > nums[m] && nums[l] > nums[r] { 
+            // ascending subarr, or mid on not rotated side
+            r = m - 1
+        } else { 
+            // mid on rotated side
+            l = m + 1
+        }
+	}
+    return result
+}
+
+// 33. Search in Rotated Sorted Array
+func Search(nums []int, target int) int {
+    l, r, m := 0, len(nums) - 1, 0
+
+    for l <= r {
+        m = (l + r) / 2
+        if nums[m] == target {
+            return m
+        }
+
+        if nums[l] <= nums[m] {
+            // left portion
+            if target > nums[m] {
+                // left portion, t > m: right
+                l = m + 1
+            } else {
+                // left portion, t < m:
+                if target >= nums[l] {
+                    // t >= l: left
+                    r = m - 1
+                } else {
+                    // else: right
+                    l = m + 1
+                }
+            }
+        } else {
+            // not rotated
+            if target < nums[m] {
+                // right portion, t < m: left
+                r = m - 1
+            } else {
+                // right portion, t > m: 
+                if target <= nums[r] {
+                    // t <= r: right
+                    l = m + 1
+                } else {
+                    // else: left
+                    r = m - 1
+                }
+            }
+        }
+    }
+
+    return -1
+}
+
+// 981. Time Based Key-Value Store
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Set(key,value,timestamp);
+ * param_2 := obj.Get(key,timestamp);
+ */
+type TimeItem struct {
+    ts int
+    data string
+}
+type TimeMap struct {
+    m map[string][]TimeItem
+}
+
+func TMapConstructor() TimeMap {
+    return TimeMap{
+        m: make(map[string][]TimeItem),
+    }
+}
+
+func (this *TimeMap) Set(key string, value string, timestamp int)  {
+    vals := this.m[key]
+    if vals == nil {
+        vals = []TimeItem{}
+    } 
+    this.m[key] = append(vals, TimeItem{
+        ts: timestamp,
+        data: value,
+    })
+}
+
+func (this *TimeMap) Get(key string, timestamp int) string {
+    vals := this.m[key]
+    if vals == nil {
+        return ""
+    }
+
+    l, r, m := 0, len(vals)-1, 0
+    for l <= r {
+        m = (l + r) / 2
+        vm := vals[m]
+        if vm.ts == timestamp {
+            return vm.data
+        }
+        if l == r {
+            if vm.ts < timestamp {
+                return vm.data
+            } else {
+                if m-1 >= 0 {
+                    return vals[m-1].data
+                } else {
+                    return ""
+                }
+            }
+        }
+        if timestamp < vm.ts {
+            r = m - 1
+        } else {
+            l = m + 1
+        }
+    }
+
+    return ""
+}
+
+// 4. Median of Two Sorted Arrays
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+    m := int(math.Ceil(float64(len(nums1)) + float64(len(nums2))) / 2)
+}
--- src/medium/tests/bsearch_test.go
+++ src/medium/tests/bsearch_test.go
@@ -17,3 +17,47 @@ func TestSearchMatrix(t *testing.T) {
     assert.Equal(t, false, m.SearchMatrix(mtx, 13))
 }
 
+func TestMinEatingSpeed(t *testing.T) {
+    assert.Equal(t, 4, m.MinEatingSpeed([]int{3,6,7,11}, 8))
+    assert.Equal(t, 30, m.MinEatingSpeed([]int{30,11,23,4,20}, 5))
+    assert.Equal(t, 23, m.MinEatingSpeed([]int{30,11,23,4,20}, 6))
+}
+
+func TestFindMin(t *testing.T) {
+    assert.Equal(t, 1, m.FindMin([]int{3,4,5,1,2}))
+    assert.Equal(t, 0, m.FindMin([]int{4,5,6,7,0,1,2}))
+    assert.Equal(t, 11, m.FindMin([]int{11,13,15,17}))
+    assert.Equal(t, 1, m.FindMin([]int{2,1}))
+
+    assert.Equal(t, 1, m.FindMinFast([]int{3,4,5,1,2}))
+    assert.Equal(t, 0, m.FindMinFast([]int{4,5,6,7,0,1,2}))
+    assert.Equal(t, 11, m.FindMinFast([]int{11,13,15,17}))
+    assert.Equal(t, 1, m.FindMinFast([]int{2,1}))
+}
+
+func TestSearchRotated(t *testing.T) {
+    assert.Equal(t, 4, m.Search([]int{4,5,6,7,0,1,2}, 0))
+    assert.Equal(t, -1, m.Search([]int{4,5,6,7,0,1,2}, 3))
+    assert.Equal(t, -1, m.Search([]int{1}, 0))
+    assert.Equal(t, 4, m.Search([]int{4,5,6,7,8,1,2,3}, 8))
+    assert.Equal(t, 1, m.Search([]int{3,1}, 1))
+}
+
+func TestTimeMap(t *testing.T) {
+    timeMap := m.TMapConstructor()
+    timeMap.Set("foo", "bar", 1)
+    assert.Equal(t, "bar", timeMap.Get("foo", 1))
+    assert.Equal(t, "bar", timeMap.Get("foo", 3))
+    timeMap.Set("foo", "bar2", 4)
+    assert.Equal(t, "bar2", timeMap.Get("foo", 4))
+    assert.Equal(t, "bar2", timeMap.Get("foo", 5))
+
+    timeMap2 := m.TMapConstructor()
+    timeMap2.Set("love", "high", 10)
+    timeMap2.Set("love", "low", 20)
+    assert.Equal(t, "", timeMap2.Get("love", 5))
+    assert.Equal(t, "high", timeMap2.Get("love", 10))
+    assert.Equal(t, "high", timeMap2.Get("love", 15))
+    assert.Equal(t, "low", timeMap2.Get("love", 20))
+    assert.Equal(t, "low", timeMap2.Get("love", 25))
+}
